c7a9ca972007d95244601128705c2a609eb0ef6c
B1329047 <52613neko@gmail.com>
2025-02-27 14:05:03 +0800

    Create main3.c

---
+++ qz1/main3.c
@@ -0,0 +1,10 @@
+#inlcude <stdio.h>
+
+int main(){
+  unsighed char i = 200;
+  unsighed char j = 220;
+  unsighed char k = 0;
+  k = ((j - i) / 2) + i;
+  printf("(%d +  %d)/2 = %d\n, i, j, k);
+  return 0;
+)
